4e00d564f0fc65e74c109fa6616d2e666ec3e2b9
X5mostafa5X <101727390+mostafa20021212@users.noreply.github.com>
2025-01-08 08:16:10 +0200

    3309-COUNT-PREFIX-AND-SUFFIX-PAIRS-I Stats: Time: 3 ms (71.43%), Space: 52.4 MB (31.43%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.ts
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.ts
@@ -1,11 +1,7 @@
 function countPrefixSuffixPairs(words: string[]): number {
-   return words.reduce((result: number, word1: string, i: number) => {
-  const matches = words.slice(i+1).reduce((count: number, word2: string, j: number) => {
-    if (   word2.startsWith(word1) && word2.endsWith(word1)) {
-      return count + 1;
-    }
-    return count;
-  }, 0);
-  return result + matches;
-}, 0);
+    return words.reduce((result: number, word1: string, i: number) => {
+        return result + words.slice(i + 1).reduce((count: number, word2: string, j: number) => {
+            return count + (word2.startsWith(word1) && word2.endsWith(word1) ? 1 : 0)
+        }, 0);
+    }, 0);
 };
